3a1c308983e50e8105205919deddb5ea4a776c0b
Mohab Safey El Din <mohab.safey@lip6.fr>
2023-01-26 10:54:58 +0100

    doc -q option

--- src/msolve/main.c
+++ src/msolve/main.c
@@ -101,7 +101,8 @@ static inline void display_help(char *str){
   fprintf(stdout, "         Default is 0. For a detailed description of the output\n");
   fprintf(stdout, "         format please see the general output data format section\n");
   fprintf(stdout, "         above.\n");
-  fprintf(stdout, "-q Q     qqqq.\n");
+  fprintf(stdout, "-q Q     Uses signature-based algotithms.\n");
+  fprintf(stdout, "         Default: 0 (no).\n");
   fprintf(stdout, "-r RED   Reduce Groebner basis.\n");
   fprintf(stdout, "         Default: 1 (yes).\n");
   /* fprintf(stdout, "-R       Refinement fo real roots.\n"); */

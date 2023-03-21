a37a7998d6d14ea8d690e311014e60a0c51a75a7
Mohab Safey El Din <mohab.safey@lip6.fr>
2023-03-21 07:51:07 +0100

    adds time msg

--- src/msolve/lifting-gb.c
+++ src/msolve/lifting-gb.c
@@ -1404,6 +1404,7 @@ int msolve_gbtrace_qq(
                       int32_t pbm_file,
                       files_gb *files){
 
+  double st0 = realtime();
   uint32_t field_char = gens->field_char;
   const void *cfs = gens->mpz_cfs;
   if(gens->field_char){
@@ -1704,7 +1705,7 @@ int msolve_gbtrace_qq(
     }
   }
   if(info_level){
-    fprintf(stderr, "%d primes used\n", nprimes);
+    fprintf(stderr, "%d primes used. Elapsed time: %.2f\n", nprimes, realtime()-st0);
   }
   free_mstrace(msd, st);
   if(dlinit){

ca78a3aa231c32b03ba796f42ab42f2f8499956d
Jérémy Berthomieu <jeremy.berthomieu@lip6.fr>
2023-06-29 10:16:16 +0200

    correct files for one-qq and diff tests

--- test/diff/diff_F4SAT-31.sh
+++ test/diff/diff_F4SAT-31.sh
@@ -3,12 +3,12 @@
 file=F4SAT-31
 
 $(pwd)/msolve -f input_files/$file.ms -o test/diff/$file.res -S -g 2
-if [ ! $? ]; then
+if [ $? -gt 0 ]; then
     exit 1
 fi
 
 diff test/diff/$file.res output_files/$file.res
-if [ !  $? ]; then
+if [ $? -gt 0 ]; then
     exit 2
 fi
 
--- test/diff/diff_nonradical_shape-qq.sh
+++ test/diff/diff_nonradical_shape-qq.sh
@@ -5,12 +5,12 @@ file=nonradical_shape-qq
 source test/diff/diff_source.sh
 
 $(pwd)/msolve -f input_files/$file.ms -o test/diff/$file.res -p 1024
-if [ ! $? ]; then
+if [ $?  -gt 0 ]; then
     exit 3
 fi
 
-diff test/diff/$file.p1024.res output_files/$file.res
-if [ !  $? ]; then
+diff test/diff/$file.res output_files/$file.p1024.res
+if [ $? -gt 0 ]; then
     exit 4
 fi
 

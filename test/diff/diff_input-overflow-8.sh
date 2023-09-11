acfb7a4dd63cd1f04c187d22d54eadf40f5e0fa4
ederc <ederc@mathematik.uni-kl.de>
2023-09-11 21:20:34 +0200

    do not test 8 bit example with probabilistic f4

--- test/diff/diff_input-overflow-8.sh
+++ test/diff/diff_input-overflow-8.sh
@@ -13,14 +13,4 @@ if [ $? -gt 0 ]; then
     exit 2
 fi
 
-$(pwd)/msolve -f input_files/$file.ms -o test/diff/$file.res -l 44
-if [ $? -gt 0 ]; then
-    exit 3
-fi
-
-diff test/diff/$file.res output_files/$file.res
-if [ $? -gt 0 ]; then
-    exit 4
-fi
-
 rm test/diff/$file.res

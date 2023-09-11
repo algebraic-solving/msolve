88fea88d8c55a0e5eff284af4fa2f676faafb605
vincent.neiger <vincent.neiger@xxx.fr>
2023-09-11 16:31:48 +0200

    add license terms

--- src/upolmat/nmod_mat_poly_io.c
+++ src/upolmat/nmod_mat_poly_io.c
@@ -1,3 +1,24 @@
+/* This file is part of msolve.
+ *
+ * msolve is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * msolve is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with msolve.  If not, see <https://www.gnu.org/licenses/>
+ *
+ * Authors:
+ * Jérémy Berthomieu
+ * Christian Eder
+ * Vincent Neiger
+ * Mohab Safey El Din */
+
 #include "nmod_mat_poly.h"
 
 void nmod_mat_poly_print(const nmod_mat_poly_t matp)
--- src/upolmat/nmod_poly_mat_pmbasis.h
+++ src/upolmat/nmod_poly_mat_pmbasis.h
@@ -1,3 +1,24 @@
+/* This file is part of msolve.
+ *
+ * msolve is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * msolve is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with msolve.  If not, see <https://www.gnu.org/licenses/>
+ *
+ * Authors:
+ * Jérémy Berthomieu
+ * Christian Eder
+ * Vincent Neiger
+ * Mohab Safey El Din */
+
 /**
  * \file nmod_poly_mat_approximant.h
  * \brief Minimal approximant bases
@@ -49,7 +70,6 @@
  * Note that the latter two restrictions on the lengths of the lists are
  * assuming left approximants; for right approximants, they are swapped.
  */
-
 
 #ifndef NMOD_POLY_MAT_APPROXIMANT_H
 #define NMOD_POLY_MAT_APPROXIMANT_H

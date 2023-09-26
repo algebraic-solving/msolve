caa1b708fa3b1fe78c9b7ef4046df9be54501e2a
Jérémy Berthomieu <jeremy.berthomieu@lip6.fr>
2023-09-26 14:49:20 +0200

    remove tests with 2 threads for F4SAT due to a bug

--- test/diff/diff_F4SAT-31.sh
+++ test/diff/diff_F4SAT-31.sh
@@ -14,17 +14,6 @@ if [ $? -gt 0 ]; then
 fi
 
 $(pwd)/msolve -f input_files/$file.ms -o test/diff/$file.res -S \
-      -g 2 -t 2
-if [ $? -gt 0 ]; then
-    exit 21
-fi
-
-diff test/diff/$file.res output_files/$file.res
-if [ $? -gt 0 ]; then
-    exit 22
-fi
-
-$(pwd)/msolve -f input_files/$file.ms -o test/diff/$file.res -S \
       -g 2 -l 44
 if [ $? -gt 0 ]; then
     exit 41
@@ -33,17 +22,6 @@ fi
 diff test/diff/$file.res output_files/$file.res
 if [ $? -gt 0 ]; then
     exit 42
-fi
-
-$(pwd)/msolve -f input_files/$file.ms -o test/diff/$file.res -S \
-      -g 2 -t 2 -l 44
-if [ $? -gt 0 ]; then
-    exit 61
-fi
-
-diff test/diff/$file.res output_files/$file.res
-if [ $? -gt 0 ]; then
-    exit 62
 fi
 
 rm test/diff/$file.res

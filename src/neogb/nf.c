b9ab037852be222a6cc34f0ccb418b68ee945cf4
ederc <ederc@mathematik.uni-kl.de>
2023-10-24 14:05:51 +0200

    adds export function for normal form

--- src/neogb/nf.c
+++ src/neogb/nf.c
@@ -80,9 +80,6 @@ bs_t *core_nf(
     select_tbr(tbr, mul, 0, mat, md, md->ht, bht, NULL);
 
     symbolic_preprocessing(mat, bs, md);
-    if (md->info_level > 1) {
-        printf("nf computation data");
-    }
     convert_hashes_to_columns(mat, md, md->ht);
     sort_matrix_rows_decreasing(mat->rr, mat->nru);
 
@@ -112,4 +109,116 @@ bs_t *core_nf(
     *errp = 0;
 
     return tbr;
+}
+
+int64_t export_nf(
+        void *(*mallocp) (size_t),
+        /* return values */
+        int32_t *nf_ld,   /* basis load */
+        int32_t **nf_len, /* length of each poly in basis */
+        int32_t **nf_exp, /* basis exponent vectors */
+        void **nf_cf,     /* coefficients of basis elements */
+        /* input values */
+        const int32_t nr_tbr_gens,
+        const int32_t *tbr_lens,
+        const int32_t *tbr_exps,
+        const void *tbr_cfs,
+        const int32_t nr_bs_gens,
+        const int32_t *bs_lens,
+        const int32_t *bs_exps,
+        const void *bs_cfs,
+        const uint32_t field_char,
+        const int32_t mon_order,
+        const int32_t elim_block_len,
+        const int32_t nr_vars,
+        const int32_t bs_is_gb,
+        const int32_t nr_threads,
+        const int32_t info_level
+        )
+{
+    /* timings */
+    double ct = cputime();
+    double rt = realtime();
+
+    /* data structures for basis, hash table and statistics */
+    bs_t *bs  = NULL;
+    bs_t *tbr = NULL;
+    md_t *md  = NULL;
+    ht_t *bht = NULL;
+
+    int32_t err = 0;
+    int success = 0;
+
+    /* get polys w.r.t. which we reduce */
+    success = initialize_gba_input_data(&bs, &bht, &md,
+            bs_lens, bs_exps, bs_cfs, field_char, mon_order, elim_block_len,
+            nr_vars, nr_bs_gens, 0, 17,
+            nr_threads, 0, 0, 44, 0,
+            1, 0, info_level);
+
+    /* all input generators are invalid */
+    if (success == -1) {
+        return_zero(nf_ld, nf_len, nf_exp, nf_cf, nr_vars, field_char, mallocp);
+        return 1;
+    }
+    if (success == 0) {
+        printf("Bad input data, stopped computation.\n");
+        exit(1);
+    }
+    if (bs_is_gb == 1) {
+        for (len_t k = 0; k < bs->ld; ++k) {
+            bs->lmps[k] = k;
+            bs->lm[k]   = bht->hd[bs->hm[k][OFFSET]].sdm;
+            bs->lml     = bs->ld;
+        }
+    } else {
+        /* compute a gb for initial generators */
+        bs = core_gba(bs, md, &err, md->fc);
+
+        if (err) {
+            printf("Problem with F4, stopped computation.\n");
+            exit(1);
+        }
+    }
+    /* initialize data for elements to be reduced,
+     * NOTE: Don't initialize BEFORE running core_f4, bht may
+     * change, so hash values of tbr may become wrong. */
+    tbr = initialize_basis(md);
+    tbr->ht = bht;
+    import_input_data(tbr, md, 0, nr_tbr_gens,
+            tbr_lens, tbr_exps, (void *)tbr_cfs, NULL);
+    tbr->ld = tbr->lml  =  nr_tbr_gens;
+
+    /* generate array for storing multiplier for polynomial
+     * to be reduced by basis */
+    exp_t *mul  = (exp_t *)calloc(bht->evl, sizeof(exp_t));
+    /* compute normal form of last element in tbr */
+    tbr = core_nf(tbr, md, mul, bs, &err);
+
+    if (err) {
+        printf("Problem with normalform, stopped computation.\n");
+        exit(1);
+    }
+
+    int64_t nterms  = export_results_from_f4(nf_ld, nf_len, nf_exp,
+            nf_cf, mallocp, &tbr, &bht, &md);
+
+    /* timings */
+    md->nf_ctime = cputime() - ct;
+    md->nf_rtime = realtime() - rt;
+
+    get_and_print_final_statistics(stderr, md, tbr);
+
+    /* free and clean up */
+    free_shared_hash_data(bht);
+    if (tbr != NULL) {
+        free_basis_without_hash_table(&tbr);
+    }
+    if (bs != NULL) {
+        free_basis(&bs);
+    }
+    free(md);
+    md    = NULL;
+
+    return nterms;
 }

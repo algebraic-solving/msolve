cab4ed7d58c5ddc5b2114d4b9b105d29977aaeee
ederc <ederc@mathematik.uni-kl.de>
2023-12-14 14:40:38 +0100

    adds all tests back

--- test/diff/diff_reals_dim0-chgvar.sh
+++ test/diff/diff_reals_dim0-chgvar.sh
@@ -27,12 +27,12 @@ fi
 $(pwd)/msolve -f input_files/$file.ms -o test/diff/$file.res \
       -p 256 -l 2 -t 2
 if [ $? -gt 0 ]; then
-    exit 3
+    exit 5
 fi
 
 diff test/diff/$file.res output_files/$file.p256.res
 if [ $? -gt 0 ]; then
-    exit 4
+    exit 6
 fi
 
 $(pwd)/msolve -f input_files/$file.ms -o test/diff/$file.res \
@@ -66,6 +66,28 @@ fi
 diff test/diff/$file.res output_files/$file.res
 if [ $? -gt 0 ]; then
     exit 62
+fi
+
+$(pwd)/msolve -f input_files/$file.ms -o test/diff/$file.res \
+      -p 256 -l 44 -t 1
+if [ $? -t 0 ]; then
+    exit 63
+fi
+
+diff test/diff/$file.res output_files/$file.p256.res
+if [ $? -gt 0 ]; then
+    exit 64
+fi
+
+$(pwd)/msolve -f input_files/$file.ms -o test/diff/$file.res \
+      -p 256 -l 44 -t 2
+if [ $? -gt 0 ]; then
+    exit 65
+fi
+
+diff test/diff/$file.res output_files/$file.p256.res
+if [ $? -gt 0 ]; then
+    exit 66
 fi
 
 rm test/diff/$file.res

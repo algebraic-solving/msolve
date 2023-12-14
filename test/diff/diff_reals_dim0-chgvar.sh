4965e9e21608bb93d0685ce2249516f1fa4c00f8
ederc <ederc@mathematik.uni-kl.de>
2023-12-14 14:26:47 +0100

    reenables exact linear algebra higher precision test

--- test/diff/diff_reals_dim0-chgvar.sh
+++ test/diff/diff_reals_dim0-chgvar.sh
@@ -13,6 +13,28 @@ if [ $? -gt 0 ]; then
     exit 2
 fi
 
+(pwd)/msolve -f input_files/$file.ms -o test/diff/$file.res \
+      -p 256 -l 2 -t 1
+if [ $? -gt 0 ]; then
+    exit 3
+fi
+
+diff test/diff/$file.res output_files/$file.p256.res
+if [ $? -gt 0 ]; then
+    exit 4
+fi
+
+(pwd)/msolve -f input_files/$file.ms -o test/diff/$file.res \
+      -p 256 -l 2 -t 2
+if [ $? -gt 0 ]; then
+    exit 3
+fi
+
+diff test/diff/$file.res output_files/$file.p256.res
+if [ $? -gt 0 ]; then
+    exit 4
+fi
+
 $(pwd)/msolve -f input_files/$file.ms -o test/diff/$file.res \
       -l 2 -t 2
 if [ $? -gt 0 ]; then

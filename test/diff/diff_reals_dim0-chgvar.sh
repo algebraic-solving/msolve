5853e4688ead32e66f2e85e92234be60847e9d8a
ederc <ederc@mathematik.uni-kl.de>
2023-12-14 14:46:50 +0100

    probabilistic f4 test only single core

--- test/diff/diff_reals_dim0-chgvar.sh
+++ test/diff/diff_reals_dim0-chgvar.sh
@@ -79,15 +79,4 @@ if [ $? -gt 0 ]; then
     exit 64
 fi
 
-$(pwd)/msolve -f input_files/$file.ms -o test/diff/$file.res \
-      -p 256 -l 44 -t 2
-if [ $? -gt 0 ]; then
-    exit 65
-fi
-
-diff test/diff/$file.res output_files/$file.p256.res
-if [ $? -gt 0 ]; then
-    exit 66
-fi
-
 rm test/diff/$file.res

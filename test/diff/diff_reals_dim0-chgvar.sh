fbce8831052ccc725a8e562a459057554a63e809
ederc <ederc@mathematik.uni-kl.de>
2023-12-14 19:11:21 +0100

    removes 256 bit precision tests

--- test/diff/diff_reals_dim0-chgvar.sh
+++ test/diff/diff_reals_dim0-chgvar.sh
@@ -14,28 +14,6 @@ if [ $? -gt 0 ]; then
 fi
 
 $(pwd)/msolve -f input_files/$file.ms -o test/diff/$file.res \
-      -p 256 -l 2 -t 1
-if [ $? -gt 0 ]; then
-    exit 3
-fi
-
-diff test/diff/$file.res output_files/$file.p256.res
-if [ $? -gt 0 ]; then
-    exit 4
-fi
-
-$(pwd)/msolve -f input_files/$file.ms -o test/diff/$file.res \
-      -p 256 -l 2 -t 2
-if [ $? -gt 0 ]; then
-    exit 5
-fi
-
-diff test/diff/$file.res output_files/$file.p256.res
-if [ $? -gt 0 ]; then
-    exit 6
-fi
-
-$(pwd)/msolve -f input_files/$file.ms -o test/diff/$file.res \
       -l 2 -t 2
 if [ $? -gt 0 ]; then
     exit 21

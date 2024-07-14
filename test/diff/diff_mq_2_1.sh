b8570050bc730187ffd4a7f643747caef0556910
safey <mohab.safey@lip6.fr>
2024-07-14 21:40:49 +0200

    remvoes debug msg

--- test/diff/diff_mq_2_1.sh
+++ test/diff/diff_mq_2_1.sh
@@ -5,52 +5,44 @@ file=mq_2_1
 $(pwd)/msolve -f input_files/$file.ms -o test/diff/$file.res \
       -l 2 -t 1
 if [ $? -gt 0 ]; then
-    echo "1"
     exit 1
 fi
 
 diff test/diff/$file.res output_files/$file.res
 if [ $? -gt 0 ]; then
-    echo "2"
     exit 2
 fi
 
 $(pwd)/msolve -f input_files/$file.ms -o test/diff/$file.res \
       -l 2 -t 2
 if [ $? -gt 0 ]; then
-    echo "21"
     exit 21
 fi
 
 diff test/diff/$file.res output_files/$file.res
 if [ $? -gt 0 ]; then
-    echo "22"
     exit 22
 fi
 
 $(pwd)/msolve -f input_files/$file.ms -o test/diff/$file.res \
       -l 44 -t 1
 if [ $? -gt 0 ]; then
-    echo "41"
     exit 41
 fi
 
 diff test/diff/$file.res output_files/$file.res
 if [ $? -gt 0 ]; then
-    echo "42"
     exit 42
 fi
 
 $(pwd)/msolve -f input_files/$file.ms -o test/diff/$file.res \
       -l 44 -t 2
 if [ $? -gt 0 ]; then
-    echo "61"
     exit 61
 fi
 
 diff test/diff/$file.res output_files/$file.res
 if [ $? -gt 0 ]; then
-    echo "62"
     exit 62
 fi
 

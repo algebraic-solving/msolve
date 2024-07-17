34bb766881da27b975ba022f3decbbf662e4f32a
safey <mohab.safey@lip6.fr>
2024-07-17 18:04:45 +0200

    tests for the new code

--- test/diff/diff_eco6-qq.sh
+++ test/diff/diff_eco6-qq.sh
@@ -14,6 +14,17 @@ if [ $? -gt 0 ]; then
 fi
 
 $(pwd)/msolve -f input_files/$file.ms -o test/diff/$file.res \
+      -L 1 -d 4 -P 2 -l 2 -t 1
+if [ $? -gt 0 ]; then
+    exit 101
+fi
+
+diff test/diff/$file.res output_files/$file.res
+if [ $? -gt 0 ]; then
+    exit 201
+fi
+
+$(pwd)/msolve -f input_files/$file.ms -o test/diff/$file.res \
       -d 4 -P 2 -l 2 -t 2
 if [ $? -gt 0 ]; then
     exit 21
@@ -22,6 +33,17 @@ fi
 diff test/diff/$file.res output_files/$file.res
 if [ $? -gt 0 ]; then
     exit 22
+fi
+
+$(pwd)/msolve -f input_files/$file.ms -o test/diff/$file.res \
+      -L 1 -d 4 -P 2 -l 2 -t 2
+if [ $? -gt 0 ]; then
+    exit 211
+fi
+
+diff test/diff/$file.res output_files/$file.res
+if [ $? -gt 0 ]; then
+    exit 221
 fi
 
 $(pwd)/msolve -f input_files/$file.ms -o test/diff/$file.res \
@@ -36,6 +58,17 @@ if [ $? -gt 0 ]; then
 fi
 
 $(pwd)/msolve -f input_files/$file.ms -o test/diff/$file.res \
+      -L 1 -d 4 -P 2 -l 44 -t 1
+if [ $? -gt 0 ]; then
+    exit 411
+fi
+
+diff test/diff/$file.res output_files/$file.res
+if [ $? -gt 0 ]; then
+    exit 421
+fi
+
+$(pwd)/msolve -f input_files/$file.ms -o test/diff/$file.res \
       -d 4 -P 2 -l 44 -t 2
 if [ $? -gt 0 ]; then
     exit 61
@@ -44,6 +77,17 @@ fi
 diff test/diff/$file.res output_files/$file.res
 if [ $? -gt 0 ]; then
     exit 62
+fi
+
+$(pwd)/msolve -f input_files/$file.ms -o test/diff/$file.res \
+      -L 1 -d 4 -P 2 -l 44 -t 2
+if [ $? -gt 0 ]; then
+    exit 611
+fi
+
+diff test/diff/$file.res output_files/$file.res
+if [ $? -gt 0 ]; then
+    exit 621
 fi
 
 rm test/diff/$file.res

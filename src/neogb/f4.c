7d0ae752b5ad7f7985bde93ab9d943a02ccf08ae
safey <mohab.safey@lip6.fr>
2024-07-23 08:09:56 +0200

    free_f4_julia_result_data modified to handle charac. 0

--- src/neogb/f4.c
+++ src/neogb/f4.c
@@ -94,12 +94,12 @@ void free_f4_julia_result_data(
 
     /* coefficients */
     if (field_char == 0) {
-        /* mpz_t **cfs = (mpz_t **)bcf;
-         * for (i = 0; i < len; ++i) {
-         *     mpz_clear((*cfs)[i]);
-         * }
-         * (*freep)(*cfs);
-         * *cfs  = NULL; */
+         mpz_t **cfs = (mpz_t **)bcf;
+         for (i = 0; i < len; ++i) {
+             mpz_clear((*cfs)[i]);
+         }
+         (*freep)(*cfs);
+         *cfs  = NULL; 
     } else {
         if (field_char > 0) {
             int32_t *cfs  = *((int32_t **)bcf);

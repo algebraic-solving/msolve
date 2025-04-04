4aa5c7a0be534bcacb8cbbb0eca69f62c133316c
safey <mohab.safey@lip6.fr>
2025-04-04 08:26:52 +0200

    removes duplication of shared hash table

--- src/msolve/msolve-data.c
+++ src/msolve/msolve-data.c
@@ -25,11 +25,7 @@ static void initialize_mstrace(mstrace_t msd, md_t *st){
    * initialize basis
    *******************/
   msd->bs_qq = initialize_basis(st);
-  /* initialize basis hash table, update hash table, symbolic hash table */
-  /* msd->bht = initialize_basis_hash_table(st);
-  msd->tht = initialize_secondary_hash_table(msd->bht, st); */
 
-  msd->bht = msd->bs_qq->ht;
   msd->tht = NULL;
 
   /* generate array to store modular bases */
@@ -47,15 +43,7 @@ static void initialize_mstrace(mstrace_t msd, md_t *st){
   msd->leadmons_current = (int32_t**)calloc(st->nthrds, sizeof(int32_t *));
 
   /* array to store one monomial */
-  msd->mgb = calloc(msd->bht->nv, sizeof(uint32_t));
-
-  msd->blht = (ht_t **)malloc((st->nthrds) * sizeof(ht_t *));
-  msd->btht = (ht_t **)malloc((st->nthrds) * sizeof(ht_t *));
-
-  for(int i = 0; i < st->nthrds; i++){
-    msd->blht[i] = NULL;
-    msd->btht[i] = NULL;
-  }
+  msd->mgb = calloc(st->nvars, sizeof(uint32_t));
 
   mpz_init(msd->mod_p);
   mpz_set_ui(msd->mod_p, 1);
@@ -76,15 +64,11 @@ static void free_mstrace(mstrace_t msd, md_t *st){
   //  free_basis(&(msd->bs_qq));
 //  free(msd->bs_qq);
 
+  fprintf(stderr, "Check that when st->ff_bits != 0 hash table should be cleared in modgbs\n");
   /***********************************************************
     to be checked if that is to be done when st->ff_bits != 0
      This was previously done only when characteristic is zero
   ************************************************************/
-  /* free_shared_hash_data(msd->bht);
-  if(msd->bht != NULL){
-    free_hash_table(&(msd->bht));
-  } */
-  /***********************************************************/
 
   if(msd->tht!=NULL){
     free_hash_table(&(msd->tht));
@@ -124,19 +108,12 @@ static void free_mstrace(mstrace_t msd, md_t *st){
   free(msd->leadmons_current);
 
   free(msd->mgb);
-  /* starts at 1 because memory is already cleaned at i = 0 */
-  for(int i = 1; i < st->nthrds; i++){
-    if(msd->blht[i] != NULL){
-      free_hash_table((msd->blht)+i);
-    }
-  }
   for(int i = 1; i < st->nthrds; i++){
     if(msd->btht[i] != NULL){
       free_hash_table((msd->btht)+i);
     }
   }
   free(msd->btht);
-  free(msd->blht);
 
   mpz_clear(msd->mod_p);
   mpz_clear(msd->prod_p);

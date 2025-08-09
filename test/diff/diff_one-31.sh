263c5997e983068d73001ffce87eb84612243d3c
ederc <ederc@mathematik.uni-kl.de>
2025-08-09 21:55:54 +0200

    adds test case for issue 228

--- test/diff/diff_one-31.sh
+++ test/diff/diff_one-31.sh
@@ -4,4 +4,49 @@ file=one-31
 
 source test/diff/diff_source.sh
 
+$(pwd)/msolve -f input_files/$file.ms -o test/diff/$file.res \
+      -g 1 -l 2 -t 1
+if [ $? -gt 0 ]; then
+    exit 3
+fi
+
+diff test/diff/$file.res output_files/$file.g1.res
+if [ $? -gt 0 ]; then
+    exit 4
+fi
+
+$(pwd)/msolve -f input_files/$file.ms -o test/diff/$file.res \
+      -g 1 -l 2 -t 2
+if [ $? -gt 0 ]; then
+    exit 23
+fi
+
+diff test/diff/$file.res output_files/$file.g1.res
+if [ $? -gt 0 ]; then
+    exit 24
+fi
+
+$(pwd)/msolve -f input_files/$file.ms -o test/diff/$file.res \
+      -g 1 -l 44 -t 1
+if [ $? -gt 0 ]; then
+    exit 43
+fi
+
+diff test/diff/$file.res output_files/$file.g1.res
+if [ $? -gt 0 ]; then
+    exit 44
+fi
+
+$(pwd)/msolve -f input_files/$file.ms -o test/diff/$file.res \
+      -g 1 -l 44 -t 2
+if [ $? -gt 0 ]; then
+    exit 63
+fi
+
+diff test/diff/$file.res output_files/$file.g1.res
+if [ $? -gt 0 ]; then
+    exit 64
+fi
+
+rm test/diff/$file.res
 

26b6011c0bd26c1362a305e8707700c3cacf50e9
Jérémy Berthomieu <jeremy.berthomieu@lip6.fr>
2025-12-10 16:11:55 +0100

    add test for issue 230 in make check

---
+++ test/diff/diff_issue_230_squared.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+#test file for real root extraction with adaptative precision
+#For this test file, one coordinate of one solutions is very close to 0
+#msolve's real root extraction should be able to make the distinction
+
+file=issue_230_squared
+
+$(pwd)/msolve -f input_files/$file.ms -o test/diff/$file.res \
+      -P 1 -l 2 -t 1
+if [ $? -gt 0 ]; then
+    exit 1
+fi
+
+diff test/diff/$file.res output_files/$file.res
+if [ $? -gt 0 ]; then
+    exit 2
+fi
+
+$(pwd)/msolve -f input_files/$file.ms -o test/diff/$file.res \
+      -P 1 -l 2 -t 2
+if [ $? -gt 0 ]; then
+    exit 21
+fi
+
+diff test/diff/$file.res output_files/$file.res
+if [ $? -gt 0 ]; then
+    exit 22
+fi
+
+$(pwd)/msolve -f input_files/$file.ms -o test/diff/$file.res \
+      -P 1 -l 44 -t 1
+if [ $? -gt 0 ]; then
+    exit 41
+fi
+
+diff test/diff/$file.res output_files/$file.res
+if [ $? -gt 0 ]; then
+    exit 42
+fi
+
+$(pwd)/msolve -f input_files/$file.ms -o test/diff/$file.res \
+      -P 1 -l 44 -t 2
+if [ $? -gt 0 ]; then
+    exit 61
+fi
+
+diff test/diff/$file.res output_files/$file.res
+if [ $? -gt 0 ]; then
+    exit 62
+fi
+
+rm test/diff/$file.res

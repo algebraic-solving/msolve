2a342978cbaad0470cbc1f41e1b6681b79f7fd21
Jérémy Berthomieu <jeremy.berthomieu@lip6.fr>
2026-03-13 12:01:26 +0100

    make check for a nonradical ideal not in shape position requiring two runs of FGLM to verify the parametrizations

---
+++ test/diff/diff_x2y2-qq.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+file=x2y2-qq
+
+source test/diff/diff_source.sh
+
+source test/diff/diff_source-param-nonf.sh
+
+source test/diff/diff_source-param-nonf-qq.sh
+
+source test/diff/diff_source-real.sh
+
+normal_exit

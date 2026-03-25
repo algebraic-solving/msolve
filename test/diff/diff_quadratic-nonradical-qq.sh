b480cb6d2c21ef65a88bf4722a8d237ebaa5a674
Jérémy Berthomieu <jeremy.berthomieu@lip6.fr>
2026-03-25 17:23:53 +0100

    update make check

---
+++ test/diff/diff_quadratic-nonradical-qq.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+file=quadratic-nonradical-qq
+
+source test/diff/diff_source.sh
+
+source test/diff/diff_source-param-nonf.sh
+
+source test/diff/diff_source-param-nonf-qq.sh
+
+source test/diff/diff_source-real.sh
+
+normal_exit
